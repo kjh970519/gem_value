9a68750520cdba37315981ba1e3ebb5748424478
kjh970519 <115525671+kjh970519@users.noreply.github.com>
2024-11-29 16:29:34 +0900

    초기화 로직 수정

--- res/js/app.js
+++ res/js/app.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
     app = {
         key: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDAyNzUwMzUifQ.ESuJfHQxHs4D8c1ToDOoklmIEbAdqYbBRtuopwEnSMnib2nS4RLpyRJJDROv8ELKdmFj2EQmMBFpJmqMTpy5YkF77nRQTqplVN1gV-eCtXkcQAXGS-AHsOkXb8VR1SUvVY8hd0UQ67oVBtjzcbk7PQA4gH4gSgGfGrlPL41AknRN92e9rFM69ImEsUZJcbtBnv_dbxTn1Sar6UBsRk9beWX8XYSzeJpDcBq5SqtblJo_EUvWVbA2iZRxVS9d5yGVjP4UAYhB7J9JiMxl2vodvYsvxXbiRVobr8Y0z4o0O6U4NduAfdYoWvOYrWRhkgGBTvfb6jtkyxa4EHx_Ny6F0w",
         gemDatas: {
-            "1레벨 멸화의 보석": {}, "2레벨 멸화의 보석": {}, "3레벨 멸화의 보석": {}, "4레벨 멸화의 보석": {}, "5레벨 멸화의 보석": {}, "6레벨 멸화의 보석": {}, "7레벨 멸화의 보석": {}, "8레벨 멸화의 보석": {}, "9레벨 멸화의 보석": {}, "10레벨 멸화의 보석": {},
-            "1레벨 홍염의 보석": {}, "2레벨 홍염의 보석": {}, "3레벨 홍염의 보석": {}, "4레벨 홍염의 보석": {}, "5레벨 홍염의 보석": {}, "6레벨 홍염의 보석": {}, "7레벨 홍염의 보석": {}, "8레벨 홍염의 보석": {}, "9레벨 홍염의 보석": {}, "10레벨 홍염의 보석": {},
-            "1레벨 겁화의 보석": {},  "2레벨 겁화의 보석": {}, "3레벨 겁화의 보석": {}, "4레벨 겁화의 보석": {}, "5레벨 겁화의 보석": {}, "6레벨 겁화의 보석": {}, "7레벨 겁화의 보석": {}, "8레벨 겁화의 보석": {}, "9레벨 겁화의 보석": {}, "10레벨 겁화의 보석": {},
+            // "1레벨 멸화의 보석": {}, "2레벨 멸화의 보석": {}, "3레벨 멸화의 보석": {}, "4레벨 멸화의 보석": {}, "5레벨 멸화의 보석": {}, "6레벨 멸화의 보석": {}, "7레벨 멸화의 보석": {}, "8레벨 멸화의 보석": {}, "9레벨 멸화의 보석": {}, "10레벨 멸화의 보석": {},
+            // "1레벨 홍염의 보석": {}, "2레벨 홍염의 보석": {}, "3레벨 홍염의 보석": {}, "4레벨 홍염의 보석": {}, "5레벨 홍염의 보석": {}, "6레벨 홍염의 보석": {}, "7레벨 홍염의 보석": {}, "8레벨 홍염의 보석": {}, "9레벨 홍염의 보석": {}, "10레벨 홍염의 보석": {},
+            // "1레벨 겁화의 보석": {},  "2레벨 겁화의 보석": {}, "3레벨 겁화의 보석": {}, "4레벨 겁화의 보석": {}, "5레벨 겁화의 보석": {},
+            // "6레벨 겁화의 보석": {}, "7레벨 겁화의 보석": {}, "8레벨 겁화의 보석": {}, "9레벨 겁화의 보석": {}, "10레벨 겁화의 보석": {},
             "1레벨 작열의 보석": {}, "2레벨 작열의 보석": {}, "3레벨 작열의 보석": {}, "4레벨 작열의 보석": {}, "5레벨 작열의 보석": {}, "6레벨 작열의 보석": {}, "7레벨 작열의 보석": {}, "8레벨 작열의 보석": {}, "9레벨 작열의 보석": {}, "10레벨 작열의 보석": {},
         },
+        gemDatasBackup: null,
         characters: [],
         viewDatas: [],
         init: function() {
@@ -94,6 +96,7 @@ $(document).ready(function() {
             }
         },
         searchCharacter: async function(nickname) {
+            app.reset(true);
             const ep = {
                 url: `https://developer-lostark.game.onstove.com/characters/${nickname}/siblings`,
                 method: "GET"
@@ -113,6 +116,7 @@ $(document).ready(function() {
                     };
                 }
             }
+            app.gemDatasBackup = JSON.parse(JSON.stringify(app.gemDatas));
         },
         getGemPrice: async function(itemName) {
             const ep = {
@@ -130,11 +134,12 @@ $(document).ready(function() {
             }
             catch (error) { }
         },
-        reset: function() {
-            $("#nickname").val('');
+        reset: function(s=false) {
+            if (!s) $("#nickname").val('');
             $(".total-value").empty();
             $("#total_price").html(0);
             app.characters = [];
+            app.gemDatas = JSON.parse(JSON.stringify(app.gemDatasBackup));
         },
         ajax: function (ep, data=null) {
             return new Promise((resolve, reject) => {
